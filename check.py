e25f9ce184289a3c940d4f16a5d927e4972804e5
pcchencode <zxp791228@gmail.com>
2020-10-16 18:03:26 +0800

    update check

--- check.py
+++ check.py
@@ -1,2 +1,8 @@
 import sys, os
 import json
+
+with open('/Users/user/Desktop/chords_recogn/CE200_sample/CE200_sample/14/ground_truth.txt', 'r') as f:
+    # print(f.strip())
+    for line in f:
+        print(line)
+
