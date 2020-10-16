c797608380c31dfd2bf3063a4940d0d045112713
pcchencode <zxp791228@gmail.com>
2020-10-16 18:16:15 +0800

    update

--- check.py
+++ check.py
@@ -2,7 +2,6 @@ import sys, os
 import json
 
 
-
 with open('/Users/user/Desktop/chords_recogn/CE200_sample/CE200_sample/14/ground_truth.txt', 'r') as f:
     chords = []
     for line in f:
@@ -16,4 +15,4 @@ with open('/Users/user/Desktop/chords_recogn/CE200_sample/CE200_sample/14/featur
     js = json.loads(d)
 print(len(js['chroma_stft']))
 
-
+# chords 跟 feature 長度不一樣...
